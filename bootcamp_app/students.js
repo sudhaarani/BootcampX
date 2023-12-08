02a960e70884e5211a7c7aab8e31dca64e8fda65
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-07 23:17:50 -0800

    use parameterized queries when you have data that comes from an untrusted source(SQL Injection)

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv.slice(2)[0];
+const limit = process.argv.slice(2)[1];
+console.log("cohortName:", cohortName);
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv.slice(2)[0]}%'
-LIMIT ${process.argv.slice(2)[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
